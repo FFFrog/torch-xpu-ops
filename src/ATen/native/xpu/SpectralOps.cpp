a0dccb8bc18e89bf05e52344d25ba15f88cfa1eb
Cui, Yifeng <yifeng.cui@intel.com>
2024-08-16 01:42:25 -0700

    Fallback MKL XPU to CPU on Windows

--- src/ATen/native/xpu/SpectralOps.cpp
+++ src/ATen/native/xpu/SpectralOps.cpp
@@ -12,7 +12,13 @@ Tensor XPUNativeFunctions::_fft_c2c(
     bool forward) {
   TORCH_CHECK(self.is_complex());
 
+#if defined(__MKL_FALLBACK_TO_CPU)
+  Tensor out_cpu = native::_fft_c2c_mkl(
+      self.to(Device(at::kCPU)), dim, normalization, forward);
+  return out_cpu.to(Device(at::kXPU));
+#else
   return native::xpu::_fft_c2c_mkl(self, dim, normalization, forward);
+#endif // __MKL_FALLBACK_TO_CPU
 }
 
 Tensor& XPUNativeFunctions::_fft_c2c_out(
@@ -23,7 +29,15 @@ Tensor& XPUNativeFunctions::_fft_c2c_out(
     Tensor& out) {
   TORCH_CHECK(self.is_complex());
 
+#if defined(__MKL_FALLBACK_TO_CPU)
+  Tensor out_cpu = out.to(Device(at::kCPU));
+  native::_fft_c2c_mkl_out(
+      self.to(Device(at::kCPU)), dim, normalization, forward, out_cpu);
+  out.copy_(out_cpu);
+  return out;
+#else
   return native::xpu::_fft_c2c_mkl_out(self, dim, normalization, forward, out);
+#endif // __MKL_FALLBACK_TO_CPU
 }
 
 } // namespace at
