8b8d6f900047012ec5f14806682a931aa0070c91
mengfeil <mengfei.li@intel.com>
2024-09-25 10:15:25 +0800

    modify bundle source

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-source /home/sdp/intel/oneapi/setvars.sh
+export ONEAPI_ROOT=/home/sdp/intel/oneapi
+source /home/sdp/intel/oneapi/compiler/latest/env/vars.sh
+source /home/sdp/intel/oneapi/umf/latest/env/vars.sh
+source /home/sdp/intel/oneapi/mkl/latest/env/vars.sh
+source /home/sdp/intel/oneapi/pti/latest/env/vars.sh
